92729310e763aa3d3583f807f06da96d602c8843
DaisyChepyegon <daisychepyegon@gmail.com>
2022-06-08 10:53:29 +0300

    commit tree 348a36a9b9c9d1c198fb58c766d16440384c005b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob";
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer="not bob"
+
+}
+
+function overwriteBestCustomer(){
+    console.log(bestCustomer='maybe bob')
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer="max"
+    console.log(leastFavoriteCustomer="maddy")
+}
+
